69e88a603f5675f41e6c22fd61dddc43021d22fc
Ron Hatcher <ron@independent-cinemas.com>
2011-06-08 08:28:27 +0100

    First shot integration with hype doc

---
+++ templates/Cinema_Resources/cinema_hype_generated_script.js
@@ -0,0 +1,67 @@
+//	HYPE.documents["Cinema"]
+
+(function HYPE_DocumentLoader() {
+	var resourcesFolderName = "Cinema_Resources";
+	var documentName = "Cinema";
+	var documentLoaderFilename = "cinema_hype_generated_script.js";
+
+	// find the URL for this script's absolute path and set as the resourceFolderName
+	try {
+		var scripts = document.getElementsByTagName('script');
+		for(var i = 0; i < scripts.length; i++) {
+			var scriptSrc = scripts[i].src;
+			if(scriptSrc != null && scriptSrc.indexOf(documentLoaderFilename) != -1) {
+				resourcesFolderName = scriptSrc.substr(0, scriptSrc.lastIndexOf("/"));
+				break;
+			}
+		}
+	} catch(err) {	}
+
+	// load HYPE.js if it hasn't been loaded yet
+	if(typeof HYPE == "undefined") {
+		if(typeof window.HYPE_DocumentsToLoad == "undefined") {
+			window.HYPE_DocumentsToLoad = new Array();
+		}
+		window.HYPE_DocumentsToLoad.push(HYPE_DocumentLoader);
+
+		var headElement = document.getElementsByTagName('head')[0];
+		var scriptElement = document.createElement('script');
+		scriptElement.type= 'text/javascript';
+		scriptElement.src = resourcesFolderName + '/' + 'HYPE.js';
+		headElement.appendChild(scriptElement);
+		return;
+	}
+	
+	var attributeTransformerMapping = {"BorderColorRight":"ColorValueTransformer","BackgroundColor":"ColorValueTransformer","BorderWidthBottom":"PixelValueTransformer","WordSpacing":"PixelValueTransformer","BoxShadowXOffset":"PixelValueTransformer","Opacity":"FractionalValueTransformer","BorderWidthRight":"PixelValueTransformer","BorderWidthTop":"PixelValueTransformer","BoxShadowColor":"ColorValueTransformer","BorderColorBottom":"ColorValueTransformer","FontSize":"PixelValueTransformer","BorderRadiusTopRight":"PixelValueTransformer","TextColor":"ColorValueTransformer","Rotate":"DegreeValueTransformer","Height":"PixelValueTransformer","PaddingLeft":"PixelValueTransformer","BorderColorTop":"ColorValueTransformer","Top":"PixelValueTransformer","BackgroundGradientStartColor":"ColorValueTransformer","TextShadowXOffset":"PixelValueTransformer","PaddingTop":"PixelValueTransformer","BackgroundGradientAngle":"DegreeValueTransformer","PaddingBottom":"PixelValueTransformer","PaddingRight":"PixelValueTransformer","Width":"PixelValueTransformer","TextShadowColor":"ColorValueTransformer","BorderColorLeft":"ColorValueTransformer","ReflectionOffset":"PixelValueTransformer","Left":"PixelValueTransformer","BorderRadiusBottomRight":"PixelValueTransformer","LineHeight":"PixelValueTransformer","BoxShadowYOffset":"PixelValueTransformer","ReflectionDepth":"FractionalValueTransformer","BorderRadiusTopLeft":"PixelValueTransformer","BorderRadiusBottomLeft":"PixelValueTransformer","TextShadowBlurRadius":"PixelValueTransformer","TextShadowYOffset":"PixelValueTransformer","BorderWidthLeft":"PixelValueTransformer","BackgroundGradientEndColor":"ColorValueTransformer","BoxShadowBlurRadius":"PixelValueTransformer","LetterSpacing":"PixelValueTransformer"};
+
+var scenes = [{"timelines":{"kTimelineDefaultIdentifier":{"framesPerSecond":30,"animations":[],"identifier":"kTimelineDefaultIdentifier","name":"Main Timeline","duration":0}},"id":"DD688399-B907-4D33-9B57-03B171016F88-1109-00000194A4978856","sceneIndex":0,"perspective":"600px","oid":"DD688399-B907-4D33-9B57-03B171016F88-1109-00000194A4978856","initialValues":{},"name":"Home","backgroundColor":"#FFFFFF"}];
+
+var javascriptMapping = {};
+
+
+	
+	var Custom = (function() {
+	return {
+	};
+}());
+
+	
+	var hypeDoc = new HYPE();
+	
+	hypeDoc.attributeTransformerMapping = attributeTransformerMapping;
+	hypeDoc.scenes = scenes;
+	hypeDoc.javascriptMapping = javascriptMapping;
+	hypeDoc.Custom = Custom;
+	hypeDoc.currentSceneIndex = 0;
+	hypeDoc.mainContentContainerID = "cinema_hype_container";
+	hypeDoc.resourcesFolderName = resourcesFolderName;
+	hypeDoc.showHypeBuiltWatermark = 1;
+	hypeDoc.showLoadingPage = false;
+	hypeDoc.drawSceneBackgrounds = true;
+	hypeDoc.documentName = documentName;
+
+	HYPE.documents[documentName] = hypeDoc.API;
+
+	hypeDoc.documentLoad(this.body);
+}());
+
